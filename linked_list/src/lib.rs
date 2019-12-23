b7b72fd399eabcb89be565120d1bf43858335d49
Chris Coverdale <chris.coverdale24@gmail.com>
2019-12-23 23:30:19 +0000

    Cargo.toml: adding Failure crate

--- linked_list/src/lib.rs
+++ linked_list/src/lib.rs
@@ -1,4 +1,11 @@
 //! A crate that implements a LinkedList.
+extern crate failure;
+#[macro_use]
+extern crate failure_derive;
+
+pub use crate::error::Result;
 pub use crate::linked_list::LinkedList;
+
+mod error;
 mod linked_list;
 mod node;
