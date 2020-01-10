f11a7773aec2f56ebde67b6e6038c84a5d11f321
Chris Coverdale <chris.coverdale24@gmail.com>
2020-01-10 16:02:24 +0000

    doubly_linked_list: removing error.rs and unused dependencies

--- doubly_linked_list/src/error.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Fail, Debug)]
-pub enum LinkedListError {
-    #[fail(display = "Index out of bounds")]
-    IndexOutOfRangeError,
-}
-
-pub type Result<T> = std::result::Result<T, LinkedListError>;
